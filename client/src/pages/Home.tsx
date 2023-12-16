12423216db3eb8be093481c37bd273d6bd0e573b
KhDan007 <kh.dan007@gmail.com>
2023-12-16 21:03:27 +0600

    connect fonts and add first block in main page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,19 @@
+import logo_transparent from '../img/logo_transparent.png';
+
 export const Home = () => {
-   return <div className="home">
-      
-   </div>;
+   return (
+      <div className="home">
+         <div className="container">
+            <div className="d-flex flex-column text-center align-items-center">
+               <img
+                  src={logo_transparent}
+                  style={{ width: '138px' }}
+                  alt="no image"
+                  className="img-fluid"
+               />
+               <p>Получи 7.0 за 45 дней интенсивной подготовки!</p>
+            </div>
+         </div>
+      </div>
+   );
 };
