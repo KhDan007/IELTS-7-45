1d30b259e0aeff6ba3f2d73cc1d58c0f751de6a7
KhDan007 <kh.dan007@gmail.com>
2023-12-16 22:06:44 +0600

    slogan block

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,15 +4,19 @@ import '../css/Home.css';
 export const Home = () => {
    return (
       <div className="home">
-         <div className="container">
-            <div className="d-flex flex-column text-center align-items-center">
-               <img
-                  src={logo_transparent}
-                  style={{ width: '138px' }}
-                  alt="no image"
-                  className="img-fluid"
-               />
-               <p className='tilda-sans-bold'>Получи 7.0 за 45 дней интенсивной подготовки!</p>
+         <div className="slogan">
+            <div className="container mt-4">
+               <div className="d-flex flex-column text-center align-items-center">
+                  <img
+                     src={logo_transparent}
+                     style={{ width: '138px' }}
+                     alt="no image"
+                     className="img-fluid"
+                  />
+                  <p className="tilda-sans-bold slogan__text">
+                     Получи 7.0 за 45 дней интенсивной подготовки!
+                  </p>
+               </div>
             </div>
          </div>
       </div>
