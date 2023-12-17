12a9f04c2c5af3c2daaecb4c05724336f439475c
KhDan007 <kh.dan007@gmail.com>
2023-12-17 16:09:38 +0600

    make examples to react-bootstrap

--- client/src/pages/components/Examples.tsx
+++ client/src/pages/components/Examples.tsx
@@ -1,73 +1,72 @@
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import img1 from '../../img/examples/1.jpg';
 import img2 from '../../img/examples/2.jpg';
 import img3 from '../../img/examples/3.jpg';
 import img4 from '../../img/examples/4.jpg';
 import img5 from '../../img/examples/5.jpg';
 
-// import '../../css/Home.css';
-
 export const Examples = () => {
    return (
       <div className="examples">
-         <div className="row">
+         <Row>
             {/* First column for the main image */}
-            <div className="col-12 col-md-6">
+            <Col xs={12} md={6}>
                <div className="examples__imageContainer">
-                  <img
+                  <Image
                      className="w-100 h-100 examples__image"
                      src={img1}
                      alt=""
                   />
                </div>
-            </div>
+            </Col>
 
             {/* Second column for the 4 smaller images */}
-            <div className="col-12 col-md-6">
+            <Col xs={12} md={6}>
                {/* First row of smaller images */}
-               <div className="row">
-                  <div className="col-6">
+               <Row>
+                  <Col xs={6}>
                      <div className="examples__imageContainer examples__smallImage">
-                        <img
+                        <Image
                            className="w-100 h-100 examples__image"
                            src={img2}
                            alt=""
                         />
                      </div>
-                  </div>
-                  <div className="col-6">
+                  </Col>
+                  <Col xs={6}>
                      <div className="examples__imageContainer examples__smallImage">
-                        <img
+                        <Image
                            className="w-100 h-100 examples__image"
                            src={img3}
                            alt=""
                         />
                      </div>
-                  </div>
-               </div>
+                  </Col>
+               </Row>
 
                {/* Second row of smaller images */}
-               <div className="row">
-                  <div className="col-6">
+               <Row>
+                  <Col xs={6}>
                      <div className="examples__imageContainer examples__smallImage">
-                        <img
+                        <Image
                            className="w-100 h-100 examples__image"
                            src={img4}
                            alt=""
                         />
                      </div>
-                  </div>
-                  <div className="col-6">
+                  </Col>
+                  <Col xs={6}>
                      <div className="examples__imageContainer examples__smallImage">
-                        <img
+                        <Image
                            className="w-100 h-100 examples__image"
                            src={img5}
                            alt=""
                         />
                      </div>
-                  </div>
-               </div>
-            </div>
-         </div>
+                  </Col>
+               </Row>
+            </Col>
+         </Row>
       </div>
    );
 };
