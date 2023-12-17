fc66998380f04c152f876307cc2197a7f85f8903
KhDan007 <kh.dan007@gmail.com>
2023-12-17 16:22:32 +0600

    make advantages to react-bootstrap

--- client/src/pages/components/Advantages.tsx
+++ client/src/pages/components/Advantages.tsx
@@ -1,3 +1,4 @@
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import icon1 from '../../img/advantages/icon1.png';
 import icon2 from '../../img/advantages/icon2.png';
 import icon3 from '../../img/advantages/icon3.png';
@@ -8,14 +9,14 @@ import icon6 from '../../img/advantages/icon6.png';
 export const Advantages = () => {
    return (
       <div className="advantages text-center py-5">
-         <div className="container">
-            <h2 className="advatages__title tilda-sans-semibold fs-1">
+         <Container>
+            <h2 className="advatages__title tilda-sans-semibold fs-1 mb-5">
                Наши преимущества:
             </h2>
-            <div className="row d-flex justify-content-between">
-               <div className="col-6 col-md-4 px-2 pb-3">
+            <Row className="d-flex justify-content-between">
+               <Col xs={6} md={4} className="px-2 pb-3">
                   <div className="advantages__icon mb-4">
-                     <img className="img-fluid" src={icon1} alt="" />
+                     <Image className="img-fluid" src={icon1} alt="" />
                   </div>
                   <h3 className="advantages__subtitle tilda-sans-semibold fs-3 mb-3">
                      Авторская методика
@@ -24,10 +25,10 @@ export const Advantages = () => {
                      Наши учебники созданы на основе практического опыта сдачи
                      теста
                   </h4>
-               </div>
-               <div className="col-6 col-md-4 px-2 pb-3">
+               </Col>
+               <Col xs={6} md={4} className="px-2 pb-3">
                   <div className="advantages__icon mb-4">
-                     <img className="img-fluid" src={icon2} alt="" />
+                     <Image className="img-fluid" src={icon2} alt="" />
                   </div>
                   <h3 className="advantages__subtitle tilda-sans-semibold fs-3 mb-3">
                      Удобное расположение
@@ -35,10 +36,10 @@ export const Advantages = () => {
                   <h4 className="advantages__text tilda-sans-light fs-5">
                      SmartPoint, Байзакова 280
                   </h4>
-               </div>
-               <div className="col-6 col-md-4 px-2 pb-3">
+               </Col>
+               <Col xs={6} md={4} className="px-2 pb-3">
                   <div className="advantages__icon mb-4">
-                     <img className="img-fluid" src={icon3} alt="" />
+                     <Image className="img-fluid" src={icon3} alt="" />
                   </div>
                   <h3 className="advantages__subtitle tilda-sans-semibold fs-3 mb-3">
                      Пробные тесты каждую неделю
@@ -46,44 +47,44 @@ export const Advantages = () => {
                   <h4 className="advantages__text tilda-sans-light fs-5">
                      Сдаем пробные тесты каждые выходные
                   </h4>
-               </div>
-
-               <div className="col-6 col-md-4 px-2 pb-3">
+               </Col>
+               <Col xs={6} md={4} className="px-2 pb-3">
                   <div className="advantages__icon mb-4">
-                     <img className="img-fluid" src={icon1} alt="" />
+                     <Image className="img-fluid" src={icon4} alt="" />
                   </div>
                   <h3 className="advantages__subtitle tilda-sans-semibold fs-3 mb-3">
-                     Авторская методика
+                     Формат
                   </h3>
                   <h4 className="advantages__text tilda-sans-light fs-5">
-                     Наши учебники созданы на основе практического опыта сдачи
-                     теста
+                     Вы можете выбрать любой удобный формат. Как оффлайн, так и
+                     онлайн
                   </h4>
-               </div>
-               <div className="col-6 col-md-4 px-2 pb-3">
+               </Col>
+               <Col xs={6} md={4} className="px-2 pb-3">
                   <div className="advantages__icon mb-4">
-                     <img className="img-fluid" src={icon2} alt="" />
+                     <Image className="img-fluid" src={icon5} alt="" />
                   </div>
                   <h3 className="advantages__subtitle tilda-sans-semibold fs-3 mb-3">
-                     Удобное расположение
+                     Удобный график
                   </h3>
                   <h4 className="advantages__text tilda-sans-light fs-5">
-                     SmartPoint, Байзакова 280
+                     Вы можете выбрать удобный для вас график вместе с
+                     преподавателем. Утром, днем, вечером - как вам удобно
                   </h4>
-               </div>
-               <div className="col-6 col-md-4 px-2 pb-3">
+               </Col>
+               <Col xs={6} md={4} className="px-2 pb-3">
                   <div className="advantages__icon mb-4">
-                     <img className="img-fluid" src={icon3} alt="" />
+                     <Image className="img-fluid" src={icon6} alt="" />
                   </div>
                   <h3 className="advantages__subtitle tilda-sans-semibold fs-3 mb-3">
-                     Пробные тесты каждую неделю
+                     Индивидуальный подход
                   </h3>
                   <h4 className="advantages__text tilda-sans-light fs-5">
-                     Сдаем пробные тесты каждые выходные
+                     Индивидуальный подход к студенту
                   </h4>
-               </div>
-            </div>
-         </div>
+               </Col>
+            </Row>
+         </Container>
       </div>
    );
 };
